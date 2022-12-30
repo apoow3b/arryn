5867938109dcf75dd3554e827b867ad2d8aeffb6
Aposh <73492847+apoow3b@users.noreply.github.com>
2022-12-30 21:05:54 +0100

    Update the website URL

--- docusaurus.config.js
+++ docusaurus.config.js
@@ -8,7 +8,7 @@ const darkCodeTheme = require('prism-react-renderer/themes/dracula');
 const config = {
   title: 'Wiki Arryn',
   tagline: 'Serveur Minecraft 1.19.2',
-  url: 'https://arryn.netlify.app',
+  url: 'https://arryn.ml',
   baseUrl: '/',
   onBrokenLinks: 'throw',
   onBrokenMarkdownLinks: 'warn',
